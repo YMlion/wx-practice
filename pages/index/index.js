e262ad7a808bfd5c71a03481fb86ae909fb0c45c
YMlion <lmion.yao@gmail.com>
2018-07-06 20:53:39 +0800

    ..

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,37 +2,12 @@
 //获取应用实例
 const app = getApp()
 var images = []
-
-var refresh = function(that, pullDown) {
-  wx.request({
-    url: 'https://gank.io/api/data/%E7%A6%8F%E5%88%A9/30/1',
-    success: function(res) {
-      console.log('load success.')
-      if (pullDown) {
-        wx.stopPullDownRefresh()
-      }
-      images = res.data.results
-      var imgsLeft = []
-      var imgsRigth = []
-      for (var i = 0; i < images.length; i++) {
-        if (i % 2 == 0) {
-          imgsLeft.push(images[i])
-        } else {
-          imgsRigth.push(images[i])
-        }
-      }
-      that.setData({
-        imagesLeft: imgsLeft,
-        imagesRight: imgsRigth
-      })
-    }
-  })
-}
+var pageNum = 1
 
 Page({
   data: {},
   //事件处理函数
-  imageTap: function(event) {
+  imageTap: function (event) {
     // just need date in fact.
     var date = event.currentTarget.dataset.item.publishedAt.substring(0, 10).replace(/-/g, '/')
     console.log(date)
@@ -41,10 +16,99 @@ Page({
     })
   },
 
-  onLoad: function() {
-    refresh(this, false)
+  onLoad: function (options) {
+    if (options && options.url) {
+      this.setData({
+        largeImageUrl: options.url,
+        showLarge: true
+      })
+    }
+    this.refresh(this, false)
+
   },
-  onPullDownRefresh: function() {
-    refresh(this, true)
+  onPullDownRefresh: function () {
+    this.refresh(this, true)
+  },
+  onReachBottom: function () {
+    this.refresh(this, false, true)
+  },
+
+  refresh: function (that, pullDown, loadMore = false) {
+    if (pullDown) {
+      pageNum = 1
+    } else if (loadMore) {
+      pageNum++
+    }
+    wx.request({
+      url: 'https://gank.io/api/data/%E7%A6%8F%E5%88%A9/10/' + pageNum,
+      success: function (res) {
+        console.log('load success.')
+        if (pullDown) {
+          wx.stopPullDownRefresh()
+        }
+        if (!loadMore) {
+          images.length = 0
+        }
+        images.push(...res.data.results)
+        var imgsLeft = []
+        var imgsRigth = []
+        for (var i = 0; i < images.length; i++) {
+          if (i % 2 == 0) {
+            imgsLeft.push(images[i])
+          } else {
+            imgsRigth.push(images[i])
+          }
+        }
+        that.setData({
+          imagesLeft: imgsLeft,
+          imagesRight: imgsRigth
+        })
+      }
+    })
+  },
+
+  onShareAppMessage: function (res) {
+    if (res.from === 'button') {
+      var imgUrl = this.data.largeImageUrl
+      return {
+        title: 'beautiful girl',
+        path: '/pages/index/index?url=' + imgUrl,
+        imageUrl: imgUrl
+      }
+    } else {
+      return {
+        title: 'beautiful girl',
+        path: '/pages/index/index',
+      }
+    }
+  },
+  showLargePicture: function (event) {
+    var url = event.currentTarget.dataset.item.url
+    this.setData({
+      largeImageUrl: url,
+      showLarge: true
+    })
+  },
+  cancelDialog: function () {
+    this.setData({
+      largeImageUrl: '',
+      showLarge: false
+    })
+  },
+  saveImage: function (event) {
+    var that = this
+    console.log(event.currentTarget.dataset.url)
+    wx.downloadFile({
+      url: event.currentTarget.dataset.url.replace('http:', 'https:'),
+      success: function (res) {
+        console.log(res.tempFilePath)
+        wx.saveImageToPhotosAlbum({
+          filePath: res.tempFilePath,
+          success: function () {
+            that.cancelDialog()
+          }
+        })
+      }
+    })
   },
 })
